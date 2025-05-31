9bcd094a4acc410b607330124c75d597dd612b91
ghulammustafa <ghulammustafamahmoodi@gmail.com>
2025-05-31 20:11:53 +0430

    update the node modules

--- company-register-api/index.js
+++ company-register-api/index.js
@@ -1,14 +1,17 @@
-const jsonServer = require("json-server");
+// index.js
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json'); // فایل داده‌ها
 const middlewares = jsonServer.defaults();
-
-const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use("/api", router);
 
+// اگر خواستی می‌تونی اینجا middleware یا قوانین خودت رو اضافه کنی
+
+server.use('/api', router);
+
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log("JSON Server is running");
+  console.log(`JSON Server is running on port ${port}`);
 });
